008c9ddf23abaea354d6ecfca4af8b951b8fe661
zhwei <sdutzhangwei@gmail.com>
2014-05-26 20:09:53 +0800

    init

---
+++ README.md
@@ -0,0 +1,7 @@
+# webhooks
+
+## 使用说明
+
+1. 创建配置文件`project_name.json`
+2. 创建重启应用的shell文件`project_name.sh`
+3. 运行时的标准错误和标准输出都会重定向到logs目录下`project_name.log`文件中
---
+++ webhook.py
@@ -0,0 +1,78 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import os
+import sys
+reload(sys)
+sys.setdefaultencoding('utf-8')
+import json
+import subprocess
+# from datetime import datetime
+import logging
+# import logging.handlers
+
+import bottle
+from bottle import request
+
+BASE = os.path.abspath(os.path.dirname(__file__))
+
+def log_handler(log_path):
+    """ create logger """
+    logger=logging.getLogger("webhook")
+    logger.setLevel(logging.WARNING)
+
+    handler = logging.FileHandler(log_path)
+    formatter = logging.Formatter('%(asctime)s-%(levelname)s:%(message)s')
+    handler.setFormatter(formatter)
+
+    logger.addHandler(handler)
+    return logger
+
+@bottle.route("/")
+def index():
+
+    return "hello"
+
+@bottle.route("/hook/<project>/", method="GET")
+@bottle.route("/hook/<project>/", method="POST")
+def receive(project):
+
+    if request.method == "POST":
+
+        config_file = "./configs/%s.json" % project
+        bash_file = "./shell/%s.sh" % project
+        log_file = os.path.join(BASE, "logs/%s.log" % project)
+
+        try:
+            with open(config_file) as fi:
+                con_json = json.loads(fi.read())
+        except IOError:
+            pass
+
+        if request.json['action'] == "push":
+
+            proc = subprocess.Popen(bash_file,
+                stdout=subprocess.PIPE,
+                stderr=subprocess.STDOUT, shell=True)
+
+            logger = log_handler(log_file)
+
+            for line in proc.stdout:
+                logger.error(line)
+            if proc.stderr is not None:
+                for line in proc.stderr:
+                    logger.error(line)
+
+
+        return json.dumps({"status": True})
+
+    return project
+
+
+
+def dev_server():
+    bottle.run(host='0.0.0.0', port=8080, debug=True)
+
+if '__main__' == __name__:
+    from django.utils import autoreload
+    autoreload.main(dev_server)
